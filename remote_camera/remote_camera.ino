6f585bef6aa968186b12a8b8784f44414abe65eb
Elie <eli.georgieva.06@gmail.com>
2020-05-14 10:36:54 +0300

    added taking n-photos, intervals between each shot and delay before taking the fist shot of the series

--- remote_camera/remote_camera.ino
+++ remote_camera/remote_camera.ino
@@ -1,7 +1,9 @@
 #include <Dabble.h>
-#define CUSTOM_SETTINGS
-#define INCLUDE_GAMEPAD_MODULE
 int LEDpin = 12;
+int interval = 1000;
+int photosNum = 1;
+boolean firstShot = false;
+int secondsBeforeFirstPhoto = 1000;
 
 void setup() {
   Serial.begin(9600);
@@ -12,44 +14,71 @@ void setup() {
 void loop() {
   Dabble.processInput();         
   if (GamePad.isUpPressed())
-     {
-         takePhoto();
-    }
-    if (GamePad.isDownPressed())
-     {
-        digitalWrite(LEDpin, LOW);
-    }
+  {
+      interval = interval + 1000;
+      Serial.print("intervals"); 
+  }
+  if (GamePad.isDownPressed())
+  {
+     takeNPhotos(photosNum, interval);
+     Serial.print("opa");  
+  }
+  if (GamePad.isLeftPressed())
+  {
+      Serial.print("photo"); 
+      photosNum++;
+  }
+  if (GamePad.isRightPressed())
+  {
+            Serial.print("photo"); 
+
+      secondsBeforeFirstPhoto = secondsBeforeFirstPhoto + 1000;
+  }
 }
 
-//execute signals to trigger the camera ir receiver 
-void takePhoto(void) {
-  int i;
-  for (i = 0; i < 76; i++) {
-  digitalWrite(LEDpin, HIGH);
-  delayMicroseconds(7);
-  digitalWrite(LEDpin, LOW);
-  delayMicroseconds(7);
+void takeNPhotos(int photos, int interval) {
+  firstShot = true;
+  for(byte i = 0; i < photos; i++) {    
+    takePhoto(secondsBeforeFirstPhoto);        // take the picture
+    delay(interval);   
+    Serial.println("snap");
   }
-  delay(27);
-  delayMicroseconds(810);
-  for (i = 0; i < 16; i++) {
-  digitalWrite(LEDpin, HIGH);
-  delayMicroseconds(7);
-  digitalWrite(LEDpin, LOW);
-  delayMicroseconds(7);
+  firstShot = false;
+  photosNum = 1;
+  interval = 1000;
+  secondsBeforeFirstPhoto = 1000;
+}
+
+void pulseON(int pulseTime) {
+  unsigned long endPulse = micros() + pulseTime;        
+  while(micros() < endPulse) {
+    digitalWrite(LEDpin, HIGH);                       
+    delayMicroseconds(13);                              
+    digitalWrite(LEDpin, LOW);                        
+    delayMicroseconds(13);                             
   }
-  delayMicroseconds(1540);
-  for (i = 0; i < 16; i++) {
-  digitalWrite(LEDpin, HIGH);
-  delayMicroseconds(7);
-  digitalWrite(LEDpin, LOW);
-  delayMicroseconds(7);
+
+}
+
+void pulseOFF(unsigned long startDelay) {
+  unsigned long endDelay = micros() + startDelay;       // create the microseconds to delay for
+  while(micros() < endDelay);
+}
+
+//executes signals that trigger the camera ir receiver
+
+void takePhoto(int delayBefore) {
+  if(firstShot == false){
+      delay(delayBefore);
   }
-  delayMicroseconds(3545);
-  for (i = 0; i < 16; i++) {
-  digitalWrite(LEDpin, HIGH);
-  delayMicroseconds(7);
-  digitalWrite(LEDpin, LOW);
-  delayMicroseconds(7);
-  }
+  for (int i=0; i < 2; i++) {
+    pulseON(2000);                                      // pulse for 2000 uS (Microseconds)
+    pulseOFF(27850);                                    // turn pulse off for 27850 us
+    pulseON(400);                                       // and so on
+    pulseOFF(1580);
+    pulseON(400);
+    pulseOFF(3580);
+    pulseON(400);
+    pulseOFF(63200);
+  }    
 }
